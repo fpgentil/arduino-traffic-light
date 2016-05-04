438a1ef81c82ee81724d53d9fcd5ff5242772a40
Felipe Gentil <cdigentil@gmail.com>
2016-05-03 21:15:55 -0300

    Simplest version of a traffic light

--- traffic-light.ino
+++ traffic-light.ino
@@ -2,8 +2,29 @@
 Traffic light project using Arduino
 */
 
+const int pinRed = 13;
+const int pinYellow = 12;
+const int pinGreen = 11;
+
+const int RED_GREEN_DELAY = 3000;
+const int YELLOW_DELAY = 1000;
+
 void setup() {
+  pinMode(pinRed, OUTPUT);
+  pinMode(pinYellow, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(pinRed, HIGH);
+  delay(RED_GREEN_DELAY);
+  digitalWrite(pinRed, LOW);
+
+  digitalWrite(pinYellow, HIGH);
+  delay(YELLOW_DELAY);
+  digitalWrite(pinYellow, LOW);
+
+  digitalWrite(pinGreen, HIGH);
+  delay(RED_GREEN_DELAY);
+  digitalWrite(pinGreen, LOW);
 }
